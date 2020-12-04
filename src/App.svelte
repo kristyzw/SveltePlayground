ecabcc18a6b34cd21d5de0b15689791bed5b3428
Wei Zhan <zhanwz@amazon.com>
2020-12-03 21:00:45 -0800

    Hello World

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = 'Max';
+  let age = 1;
+
+  function incrementAge() {
+    age += 1;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
